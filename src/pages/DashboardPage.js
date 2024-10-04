9579f05e8c1ba6339129428a6abcd27165844871
Gagnaire Pauline <gagnaire.pauline@gmail.com>
2024-10-04 16:37:12 +0200

    Ajout du premier élément de menu horizontal.
    - Ajout de la page DashboardPage.
    - Ajout du composant MenuHorizontal.
    - Ajout des styles pour le composant MenuHorizontal.
    - Suppression des logos par défaut de React.

---
+++ src/pages/DashboardPage.js
@@ -0,0 +1,12 @@
+import React from "react";
+import MenuHorizontal from "../components/MenuHorizontal";
+
+const DashboardPage = () => {
+  return (
+    <div>
+      <MenuHorizontal />
+    </div>
+  );
+};
+
+export default DashboardPage;
